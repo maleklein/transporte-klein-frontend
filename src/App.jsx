97e6bfeb17fd5660f08acb439aa5f5891dc0fb10
Malena <malena.klein@uap.edu.ar>
2026-08-12 16:32:27 -0300

    Dependencias agregadas

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
+function Home() {
+  return <h1>Transporte Klein</h1>
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
